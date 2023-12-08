5d39a43bc445c493efc51775ca4ca531eb53ff1e
Anshumansingho7 <146925992+Anshumansingho7@users.noreply.github.com>
2023-12-08 11:32:23 +0530

    Update month.rb

--- month.rb
+++ month.rb
@@ -8,7 +8,7 @@ begin
   user_date = Date.parse(user_input)
   past1 = user_date.month - 1
   puts "past month is #{past1}"
-  future = user_date.month - 1
+  future = user_date.month + 1
   puts "future month is #{future}"
 rescue ArgumentError
   puts "Invalid date format. Please enter a date in the format YYYY-MM-DD."
