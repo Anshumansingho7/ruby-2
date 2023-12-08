a84c05efc0c8617c4ba7d272007e5351ce20519a
Anshumansingho7 <146925992+Anshumansingho7@users.noreply.github.com>
2023-12-08 12:07:34 +0530

    Update month.rb

--- month.rb
+++ month.rb
@@ -3,14 +3,20 @@ require 'date'
 
 print "Enter a date (YYYY-MM-DD): "
 user_input = gets.chomp
-
+date_format = "%Y-%m-%d"
 begin
+  parsed_date = Date.strptime(user_input, date_format)
+  if parsed_date.strftime(date_format) == user_input
   user_date = Date.parse(user_input)
   past1 = user_date.month - 1
   puts "past month is #{past1}"
   future = user_date.month + 1
   puts "future month is #{future}"
+  else 
+    puts "Invalid date format. Please enter a date in the format YYYY-MM-DD."
+  end
 rescue ArgumentError
   puts "Invalid date format. Please enter a date in the format YYYY-MM-DD."
 end
+
 
