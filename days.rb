d5a8069c1cec4b9d3e1cb1a5935257c31c637534
Anshumansingho7 <146925992+Anshumansingho7@users.noreply.github.com>
2023-12-08 12:09:47 +0530

    Update days.rb

--- days.rb
+++ days.rb
@@ -4,8 +4,10 @@ require 'date'
 
 print "Enter a date (YYYY-MM-DD): "
 user_input = gets.chomp
-
+date_format = "%Y-%m-%d"
 begin
+  parsed_date = Date.strptime(user_input, date_format)
+  if parsed_date.strftime(date_format) == user_input
   user_date = Date.parse(user_input)
   past = user_date - 10
   puts "past days is #{past}"
@@ -14,10 +16,20 @@ begin
   puts dayname
   future = user_date + 10
   puts future
-  
+  else 
+    puts "Invalid date format. Please enter a date in the format YYYY-MM-DD."
+  end
 rescue ArgumentError
   puts "Invalid date format. Please enter a date in the format YYYY-MM-DD."
 end
+
+      
+
+
+
+
+
+
 
       
 
